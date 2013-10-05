ad70a8dac7f25ce33f82a556f7ce770744b4b066
Derrick Gall <derrick.gall@thomsonreuters.com>
2013-10-05 14:47:05 -0500

    Initial commit and updated README

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-trackerhacker
-=============
+Tracker Hacker
+==============
 
 Extension that enhances the Pivotal Tracker experience.
+
+## Features
+
+* Instant filtering of cards when typing in search box
+ * Can also filter by owner if you type: mywork:name\_or\_abbreviation
+ 
---
+++ js/trackerhacker.js
@@ -0,0 +1,61 @@
+setInterval(function () {
+	$('.story.item:not(.hacked)').each(function () {
+		var self = $(this),
+			keywords = [],
+			owner = [];
+
+		// Labels/epics
+		self.find('.label').each(function () {
+			keywords.push(this.innerHTML);
+		});
+
+		// Owner info
+		self.find('.owner').each(function () {
+			owner.push(this.title);
+			owner.push(this.innerHTML);
+		});
+		owner = owner.join(' ');
+		keywords.push(owner);
+
+		// Story name
+		keywords.push(self.find('.story_name').text());
+
+		self
+			.data('keywords', keywords.join(' ').toLowerCase())
+			.data('owner', owner.toLowerCase())
+			.addClass('hacked');
+
+		executeSearch();
+	});
+}, 500);
+
+function keywordFilter(value) {
+	return $(this).data('keywords').indexOf(value) < 0;
+}
+
+function ownerFilter(value) {
+	return $(this).data('owner').indexOf(value) < 0;
+}
+
+function executeSearch() {
+	var filterFunction = keywordFilter,
+		stories = $('.story.item').show(),
+		value = $.trim(this.value).toLowerCase();
+
+	if (value.indexOf(':') > -1) {
+		if (value.indexOf('mywork:') === 0) {
+			value = value.substring(7);
+			filterFunction = ownerFilter;
+		}
+	}
+
+	if (value === '') {
+		return true;
+	}
+
+	console.time('search');
+	stories.filter($.proxy(filterFunction, null, value)).hide();
+	console.timeEnd('search');
+}
+
+$(document.body).on('keyup change focus', '[name="search"]', executeSearch);
